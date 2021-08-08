1e2863f2efe59f17450ef745e2b6f06352501c43
Edi <34984294+edihasaj@users.noreply.github.com>
2021-08-08 15:13:35 +0200

    added initial files

---
+++ Sirtari.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace SirtariToshiba
+{
+    partial class Sirtari
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openCashDrawer = new System.Windows.Forms.Button();
+            this.waitCashDrawerOpen = new System.Windows.Forms.Button();
+            this.closeCashDrawer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openCashDrawer
+            // 
+            this.openCashDrawer.Location = new System.Drawing.Point(26, 20);
+            this.openCashDrawer.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.openCashDrawer.Name = "openCashDrawer";
+            this.openCashDrawer.Size = new System.Drawing.Size(154, 43);
+            this.openCashDrawer.TabIndex = 0;
+            this.openCashDrawer.Text = "Hap sirtarin";
+            this.openCashDrawer.UseVisualStyleBackColor = true;
+            this.openCashDrawer.Click += new System.EventHandler(this.openCashDrawer_Click);
+            // 
+            // waitCashDrawerOpen
+            // 
+            this.waitCashDrawerOpen.Location = new System.Drawing.Point(26, 74);
+            this.waitCashDrawerOpen.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.waitCashDrawerOpen.Name = "waitCashDrawerOpen";
+            this.waitCashDrawerOpen.Size = new System.Drawing.Size(154, 43);
+            this.waitCashDrawerOpen.TabIndex = 1;
+            this.waitCashDrawerOpen.Text = "Prit mbylljen e sirtarit";
+            this.waitCashDrawerOpen.UseVisualStyleBackColor = true;
+            this.waitCashDrawerOpen.Click += new System.EventHandler(this.waitCashDrawerOpen_Click);
+            // 
+            // closeCashDrawer
+            // 
+            this.closeCashDrawer.Location = new System.Drawing.Point(26, 127);
+            this.closeCashDrawer.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.closeCashDrawer.Name = "closeCashDrawer";
+            this.closeCashDrawer.Size = new System.Drawing.Size(154, 43);
+            this.closeCashDrawer.TabIndex = 2;
+            this.closeCashDrawer.Text = "Ndalo sirtarin";
+            this.closeCashDrawer.UseVisualStyleBackColor = true;
+            this.closeCashDrawer.Click += new System.EventHandler(this.closeCashDrawer_Click);
+            // 
+            // Sirtari
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(206, 189);
+            this.Controls.Add(this.closeCashDrawer);
+            this.Controls.Add(this.waitCashDrawerOpen);
+            this.Controls.Add(this.openCashDrawer);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Sirtari";
+            this.ShowIcon = false;
+            this.Text = "Sirtari";
+            this.Load += new System.EventHandler(this.Sirtari_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openCashDrawer;
+        private System.Windows.Forms.Button waitCashDrawerOpen;
+        private System.Windows.Forms.Button closeCashDrawer;
+    }
+}
+
---
+++ Sirtari.cs
@@ -0,0 +1,137 @@
+﻿using OposCashDrawer_CCO;
+using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace SirtariToshiba
+{
+    public partial class Sirtari : Form
+    {
+        private int _secureId = 0;
+        private readonly OPOSCashDrawer _drawer;
+
+        public Sirtari(int secureId = 0)
+        {
+            StartPosition = FormStartPosition.CenterScreen;
+            InitializeComponent();
+            _secureId = secureId;
+            _drawer = new OPOSCashDrawer();
+        }
+
+        public int OpenDrawer()
+        {
+            long cashDrawerResult = 1;
+
+            _drawer.Open("CashDrawer");
+            _drawer.ClaimDevice(200);
+            _drawer.DeviceEnabled = true;
+
+            if (_drawer.DrawerOpened)
+            {
+                _secureId = 1;
+                return _secureId;
+            }
+            else if (_secureId == 0)
+            {
+                MessageBox.Show("Për siguri, sirtari duhet të hapet manualisht herën e parë.");
+
+                int x = 0;
+                while ((!_drawer.DrawerOpened) && x++ < 5)
+                {
+                    Thread.Sleep(2000);
+                }
+
+                if (!_drawer.DrawerOpened)
+                {
+                    cashDrawerResult = 0;
+                    MessageBox.Show("Hapja e sirtarit deshtoj. Hapja manuale nuk u detektua!");
+                }
+                else
+                {
+                    _secureId = 1;
+                    MessageBox.Show("Hapja manuale u detektua, tani mbyll sirtarin.");
+
+                    int x2 = 0;
+                    while ((_drawer.DrawerOpened) && x2++ < 3)
+                    {
+                        Thread.Sleep(2000);
+                    }
+
+                    if (_drawer.DrawerOpened)
+                    {
+                        cashDrawerResult = 0;
+                        MessageBox.Show("Sirtari i mbyllur nuk u detektua.");
+                    }
+                }
+            }
+
+            if (cashDrawerResult == 1)
+            {
+                cashDrawerResult = _drawer.OpenDrawer();
+                _secureId = 1;
+            }
+
+            _drawer.DeviceEnabled = false;
+            _drawer.ReleaseDevice();
+            _drawer.Close();
+            Close();
+
+            return _secureId;
+        }
+
+        private void Sirtari_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void openCashDrawer_Click(object sender, EventArgs e)
+        {
+            OpenDrawer();
+        }
+
+        private void waitCashDrawerOpen_Click(object sender, EventArgs e)
+        {
+            WaitForCashOrderOpen();
+        }
+
+        public void WaitForCashOrderOpen()
+        {
+            long cashDrawerResult = 1;
+
+            _drawer.Open("CashDrawer");
+            _drawer.ClaimDevice(200);
+            _drawer.DeviceEnabled = true;
+
+            MessageBox.Show("Duke pritur që sirtari të mbyllet...");
+            cashDrawerResult = _drawer.WaitForDrawerClose(5000, 250, 500, 250);
+
+            if (cashDrawerResult == 1)
+            {
+                string strOutput = "Statusi i sirtarit = %s" + _drawer.DrawerOpened.ToString();
+                MessageBox.Show("Komanda për mbylljen e sirtarit u dërgua me sukses\n" + strOutput);
+            }
+            else
+            {
+                MessageBox.Show("Mbyllja e sirtarit dështoj!");
+            }
+
+            _drawer.DeviceEnabled = false;
+            _drawer.ReleaseDevice();
+            _drawer.Close();
+        }
+
+        private void closeCashDrawer_Click(object sender, EventArgs e)
+        {
+            _drawer.DeviceEnabled = false;
+            _drawer.ReleaseDevice();
+            _drawer.Close();
+            Close();
+        }
+
+        public bool HasSecureConnection()
+        {
+            if (_secureId == 1)
+                return true;
+            return false;
+        }
+    }
+}
